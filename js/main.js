0e263fc23334aac4fb59f7bc3a605d698cee2aa9
lugDroid <lugdroid@gmail.com>
2016-03-22 20:31:01 +0000

    Streams information rendered

--- js/main.js
+++ js/main.js
@@ -2,37 +2,79 @@ $(document).ready(function() {
 
   var twitchChannels = (function(){
     // list of programming related channels
-    var channels = ["freecodecamp", "storbeck", "terakilobyte", "habathcx","RobotCaleb","thomasballinger","noobs2ninjas","beohoff"];
+    var channelNames = ["freecodecamp", "storbeck", "terakilobyte", "habathcx","RobotCaleb","thomasballinger","noobs2ninjas","beohoff", "OgamingSC2", "ESL_SC2", "brunofin", "comster404", "cohhcarnage"];
+
+    // channel object template
+    var channelTemplate = {
+      name: "",
+      description: "",
+      status: "",
+      link: "",
+      image: ""
+    };
 
     // cache DOM
+    var $channelHTML = $("#channel-template").html();
 
     // bind events
+    // TODO: add buttons bindings
 
-    // get channels info from twitch.tv
-    function getChannelsInfo() {
-      // for each element in channels
-      $.getJSON("https://api.twitch.tv/kraken/streams/freecodecamp?callback=?" , parseInfo);
+    // get streams info from twitch.tv
+    function getStreamsInfo() {
+      for (var i = 0; i < channelNames.length; i++) {
+        $.getJSON("https://api.twitch.tv/kraken/streams/" + channelNames[i] + "?callback=?" , parseStreamsInfo);
+      }
     }
 
-    // parse info received and store it in array of objects
-    function parseInfo(data) {
-      // push object with info into array
+    // parse streams info received and render on screen
+    function parseStreamsInfo(data) {
+      // create new object from channelTemplate
+      var channel = Object.create(channelTemplate);
+      // populate object properties
+      if (data.error == "Unprocessable Entity") {
+        channel.name = data.message.slice(data.message.indexOf("'") + 1, data.message.lastIndexOf("'"));
+        channel.description = "Account Closed";
+        channel.image = "./img/twitch-logo.png";
+        channel.link = "https://secure.twitch.tv";
+        // render closed Account
+        render(channel);
+      } else {
+        channel.name = data._links.self.slice(data._links.self.lastIndexOf("/") + 1);
+        if (data.stream !== null) {
+          channel.description = data.stream.channel.status;
+          channel.link = data.stream.channel.url;
+          channel.image = data.stream.channel.logo;
+          // if stream is online we have all the information
+          render(channel);
+        } else {
+          $.getJSON(data._links.channel, parseChannelData.bind(channel)); // if stream is offline get channel info to obtain remaining info
+        }
+      }
     }
 
-    function render() {
-      // render list of channels
+    // parse channel info received and render on screen
+    function parseChannelData(channel, status , data) {
+      channel.description="Channel offline";
+      channel.link = data.responseJSON.url;
+      if (data.responseJSON.logo !== null) {
+        channel.image = data.responseJSON.logo;
+      } else {
+        channel.image = "./img/twitch-logo.png";
+      }
+      // now we have all the info fir these too
+      render(channel);
     }
 
+    // display information on screen
+    function render(channel) {
+        $("#main-wrapper").append(Mustache.render($channelHTML, channel));
+    }
+
+    return {
+      init: getStreamsInfo
+    };
   })();
 
   // render information received
-  twitchChannels.render();
-
-  // ####### OLD CODE ########## //
-  //for (var i = 0; i < channels.length; i++) {
-    //console.log('https://api.twitch.tv/kraken/streams/' + channels[i] + '?callback=?');
-    //$.getJSON('https://api.twitch.tv/kraken/streams/' + channels[i] + '?callback=?', function(data) {
-      //console.log(channels[i], data._links.channel);
-    //});
-  //}
+  twitchChannels.init();
 });
